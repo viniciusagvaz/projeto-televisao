26b9594c4c017d23be65c2f74355560bd9ca142c
zoomviex <agvazvinicius@gmail.com>
2023-02-08 17:32:55 -0300

    minor aditions to js: volume up/down with it's values

--- js/controle.js
+++ js/controle.js
@@ -1,4 +1,4 @@
-   const tela = document.getElementById("tv");
+   const tela = document.getElementById("tv-screen");
    let isPower = false;
    let channel = 3;
    let volume = 5;
@@ -9,22 +9,43 @@ function power(){
       isPower = true;
    } else {
       tela.style.backgroundImage = "";
+      tela.innerHTML = " "
       isPower = false;
    }
 }
 
+function home(){
+
+}
+
 function channelUp(){
-   tela.innerHTML  = `Canal +`
+  if (isPower){
+      tela.innerHTML  = `Canal +`
+   }
 }
 
 function channelDown(){
-
+  if (isPower){
+      tela.innerHTML  = `Canal -`
+   }
 }
 
 function volumeUp(){
-
+   if (isPower){
+      if(volume < 100){
+         volume += 1
+      tela.innerHTML  = `Volume ${volume}`
+      console.log(volume)
+      }
+   }
 }
 
 function volumeDown(){
-
+   if (isPower){
+      if(volume >= 1){
+         volume -= 1
+      tela.innerHTML  = `Volume ${volume}`
+      console.log(volume)
+      }
+   }
 }
