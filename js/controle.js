838ed16a7e5c3de72bd4687743b208f60ef1b6de
zoomviex <agvazvinicius@gmail.com>
2023-02-20 15:30:33 -0300

    starting design

--- js/controle.js
+++ js/controle.js
@@ -5,7 +5,7 @@
    
 function power(){
    if (!isPower){
-   tela.style.backgroundImage = "url(https://i.vimeocdn.com/video/1225013615-a36bba843bb44d1516130cf192f39fdea403821a740db08ea7f5c2d7da7ac099-d?mw=1500&mh=792&q=70)";
+   tela.style.backgroundImage = "url(https://media.tenor.com/O51PN7jtRc0AAAAC/april-5centimeters-per-second.gif)";
       isPower = true;
    } else {
       tela.style.backgroundImage = ""
@@ -20,14 +20,10 @@ function home(){
 
 function channelUp(){
   if (isPower){
-      if (channel < 2){
+      if (channel < 5){
          channel += 1
       tela.innerHTML = `Canal ${channel}`
-      tela.style.backgroundImage = "url(https://images.pexels.com/photos/15113597/pexels-photo-15113597.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-      }else if (channel < 2){
-         channel += 1
-      tela.innerHTML = `Canal ${channel}`
-      tela.style.backgroundImage = "url(https://images.pexels.com/photos/15113597/pexels-photo-15113597.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+      tela.style.backgroundImage = "url(https://media.tenor.com/88dnH_mHRLAAAAAC/static-tv-static.gif)";
       }
    }
 }
@@ -37,7 +33,7 @@ function channelDown(){
       if (channel > 1){
          channel -= 1
       tela.innerHTML = `Canal ${channel}`
-      tela.style.backgroundImage = "url(https://i.vimeocdn.com/video/1225013615-a36bba843bb44d1516130cf192f39fdea403821a740db08ea7f5c2d7da7ac099-d?mw=1500&mh=792&q=70)";
+      tela.style.backgroundImage = "url(https://media.tenor.com/O51PN7jtRc0AAAAC/april-5centimeters-per-second.gif)";
       }
    }
 }
