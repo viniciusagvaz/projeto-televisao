7ecc52450674cf0a77d61aa3a3375704d15b66e5
zoomviex <agvazvinicius@gmail.com>
2023-02-20 13:53:28 -0300

    starting to implement bootstrap

---
+++ README.md
@@ -0,0 +1,7 @@
+This project should work as if it was a tv with it's remote to change channels and turn the volume to up and down.
+
+For the time being, this project will have poor js codes since i've been studying for just a little while now.
+
+Trying my best to reduce the CSS using Bootstrap, but maybe will add the CSS later with a more robust approach to it. Will see 
+
+;)
--- js/controle.js
+++ js/controle.js
@@ -1,14 +1,14 @@
    const tela = document.getElementById("tv-screen");
    let isPower = false;
-   let channel = 13;
-   let volume = 5;
+   let channel = 1;
+   let volume = 2;
    
 function power(){
    if (!isPower){
-   tela.style.backgroundImage = "url(../img/tela-img-1.jpg)";
+   tela.style.backgroundImage = "url(https://i.vimeocdn.com/video/1225013615-a36bba843bb44d1516130cf192f39fdea403821a740db08ea7f5c2d7da7ac099-d?mw=1500&mh=792&q=70)";
       isPower = true;
    } else {
-      tela.style.backgroundImage = "";
+      tela.style.backgroundImage = ""
       tela.innerHTML = " "
       isPower = false;
    }
@@ -20,9 +20,14 @@ function home(){
 
 function channelUp(){
   if (isPower){
-      if (channel < 13){
+      if (channel < 2){
          channel += 1
       tela.innerHTML = `Canal ${channel}`
+      tela.style.backgroundImage = "url(https://images.pexels.com/photos/15113597/pexels-photo-15113597.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+      }else if (channel < 2){
+         channel += 1
+      tela.innerHTML = `Canal ${channel}`
+      tela.style.backgroundImage = "url(https://images.pexels.com/photos/15113597/pexels-photo-15113597.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
       }
    }
 }
@@ -32,6 +37,7 @@ function channelDown(){
       if (channel > 1){
          channel -= 1
       tela.innerHTML = `Canal ${channel}`
+      tela.style.backgroundImage = "url(https://i.vimeocdn.com/video/1225013615-a36bba843bb44d1516130cf192f39fdea403821a740db08ea7f5c2d7da7ac099-d?mw=1500&mh=792&q=70)";
       }
    }
 }
